512c0a7634b201fcfdd5e36ffb8f1c18ec9c366a
Blair Hagen <hagenb@union.edu>
2018-04-19 20:02:24 -0400

    Added second sorting loop, but gives wrong runtime

--- PS3_ModifiedPartition.py
+++ PS3_ModifiedPartition.py
@@ -25,8 +25,6 @@ def mod_partition(A, p, r):
 	pivot = A[r]
 
 	i = p
-	q = r
-	t = r
 
 	for j in range(p, r):
 		if A[j] <= pivot:
@@ -35,15 +33,26 @@ def mod_partition(A, p, r):
 			A[i] = temp1
 			A[j] = temp2
 			i = i+1
-		if A[j-1] == A[j]:
-			if A[q] != A[j]:
-				q = j - 1
-			t = j
-			j = j + 1
+
 	temp1 = A[i]
 	temp2 = A[r]
 	A[i] = temp2
 	A[r] = temp1
+
+	q = i
+	t = i
+	print("Initial Sort: ", A)
+	print("Pivot: ", i)
+
+	for x in range(p, i-1):
+		if A[q] == pivot:
+			q= q-1
+		elif A[x] == pivot:
+			temp1 = A[x]
+			temp2 = A[q]
+			A[x] = temp2
+			A[q] = temp1
+			q = q-1
 
 	R = [q,t]
 
@@ -57,11 +66,20 @@ if __name__ == "__main__":
 	print("All Same:")
 	A = [2,2,2,2,2,2,2,2]
 	ret = mod_partition(A, 0, 7)
-	print(A)
+	print("Final Sort: ", A)
 	print(ret)
+	print()
+
+	print("All Same:")
+	A = [1,7,2,22,35,2,4,-9,2,2]
+	ret = mod_partition(A, 0, len(A)-1)
+	print("Final Sort: ", A)
+	print(ret)
+	print()
 
 	print("None Same:")
 	A = [1,5,7,23,3,76,1,-5]
 	ret = mod_partition(A, 0, 7)
-	print(A)
+	print("Final Sort: ", A)
 	print(ret)
+	print()
