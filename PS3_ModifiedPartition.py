0340bec22af85f3a3a4eb039e24866b2442c8280
Blair Hagen <hagenb@union.edu>
2018-04-20 11:24:14 -0400

    Finished algorithm

--- PS3_ModifiedPartition.py
+++ PS3_ModifiedPartition.py
@@ -25,36 +25,33 @@ def mod_partition(A, p, r):
 	pivot = A[r]
 
 	i = p
+	z = p
 
 	for j in range(p, r):
-		if A[j] <= pivot:
-			temp1 = A[j]
-			temp2 = A[i]
-			A[i] = temp1
-			A[j] = temp2
-			i = i+1
+		if A[j] == pivot:
+			temp = A[j] # Swap A[z] with A[j]
+			A[j] = A[z]
+			A[z] = temp
 
-	temp1 = A[i]
-	temp2 = A[r]
-	A[i] = temp2
-	A[r] = temp1
+			z += 1
+		elif A[j] < pivot:
+			temp = A[j] # Swap A[j] with A[z]
+			A[j] = A[z]
+			A[z] = temp
 
-	q = i
-	t = i
-	print("Initial Sort: ", A)
-	print("Pivot: ", i)
+			temp = A[i] # Swap A[z] with A[i]
+			A[i] = A[z]
+			A[z] = temp
 
-	for x in range(p, i-1):
-		if A[q] == pivot:
-			q= q-1
-		elif A[x] == pivot:
-			temp1 = A[x]
-			temp2 = A[q]
-			A[x] = temp2
-			A[q] = temp1
-			q = q-1
+			z += 1
+			i += 1
 
-	R = [q,t]
+	# Final Swap:
+	temp = A[z] # Swap A[r] with A[z]
+	A[z] = A[r]
+	A[r] = temp
+
+	R = [i,z]
 
 	return R
 
@@ -63,23 +60,26 @@ if __name__ == "__main__":
 	# print('index ', partition(A, 0, 7))
 	# print(A)
 
-	print("All Same:")
 	A = [2,2,2,2,2,2,2,2]
-	ret = mod_partition(A, 0, 7)
+	ret = mod_partition(A, 0, len(A)-1)
 	print("Final Sort: ", A)
 	print(ret)
 	print()
 
-	print("All Same:")
 	A = [1,7,2,22,35,2,4,-9,2,2]
 	ret = mod_partition(A, 0, len(A)-1)
 	print("Final Sort: ", A)
 	print(ret)
 	print()
 
-	print("None Same:")
 	A = [1,5,7,23,3,76,1,-5]
-	ret = mod_partition(A, 0, 7)
+	ret = mod_partition(A, 0, len(A)-1)
+	print("Final Sort: ", A)
+	print(ret)
+	print()
+
+	A = [10,10,9,9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1,1,5]
+	ret = mod_partition(A, 0, len(A)-1)
 	print("Final Sort: ", A)
 	print(ret)
 	print()
